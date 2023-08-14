abb5b3a64dce3de90b583378a319094e0cf167e1
hokorizamurai <134501461+hokorizamurai@users.noreply.github.com>
2023-08-14 20:46:35 +0900

    Basic認証設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'hokori' && password == '0918'
+    end
+  end
 end
