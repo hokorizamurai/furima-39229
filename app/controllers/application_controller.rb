0714c1c48ae9ad0dda1c735573705438879cab4e
hokorizamurai <134501461+hokorizamurai@users.noreply.github.com>
2023-08-23 13:36:58 +0900

    ユーザー管理機能を搭載

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
+    def basic_auth
+      authenticate_or_request_with_http_basic do |username, password|
       username == 'hokori' && password == '0918'
     end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_of_birth])
+    end
+    
   end
 end
